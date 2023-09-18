869bc23557618e688789e2a9441a5601bf84a502
s1nyx <sinyx.contact@gmail.com>
2023-09-18 20:43:42 +0200

    feat: Added auth with clark : sign-in and sign-up routes

--- app/(auth)/(routes)/login/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Login() {
-    return (
-        <div className="text-green-500 text-3xl">Login</div>
-    )
-}
--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,5 +1,9 @@
+import { UserButton } from "@clerk/nextjs";
+
 export default function Home() {
-  return (
-    <div className="text-green-500 text-3xl">Salut</div>
-  )
+    return (
+        <div>
+            <UserButton afterSignOutUrl="/" />
+        </div>
+    )
 }
